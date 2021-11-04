5a353d81cbe5482cabe1bdf854ff4331da91c78b
unknown <putrifadyra22@gmail.com>
2021-11-04 09:24:35 +0700

    Kodingan lab 2

---
+++ 2.py
@@ -0,0 +1,11 @@
+a=int (input(" Masukkan nilai a : "))
+b=int(input(" Masukkan nilai b : "))
+print(" Variabel a = ", b)
+print(" Variabel a = ", a)
+print(" Hasil penggabungan  {1} & {0} =%d" . format(a,b) %(a+b))
+
+#konversi nilai variabel
+a=int (input(" Masukkan nilai a : "))
+b=int(input(" Masukkan nilai b : "))
+print(" Hasil penjumlahan  {1} & {0} =%d" . format(a,b) %(a+b))
+print(" Hasil pembagian  {1} & {0} =%d" . format(a,b) %(a/b))
