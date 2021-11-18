bea87d4c273ed72ec6bb86cae885f01adbb1e9d9
unknown <putrifadyra22@gmail.com>
2021-11-18 11:23:50 +0700

    Perulangan

---
+++ perulangan.py
@@ -0,0 +1,20 @@
+print(' ')
+print('Program menentukan Nilai Terbesar dari 2 bilangan')
+
+
+print(' ')
+a = int(input(" bilangan pertama: "))
+b = int(input(" bilangan kedua: "))
+
+
+print(' ')
+if a > b:
+    maks = a
+else:
+    maks = b
+
+print('Bilangan Terbesar adalah %d' % maks)
+
+
+
+
